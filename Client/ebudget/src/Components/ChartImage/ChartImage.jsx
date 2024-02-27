b66c87f2fa8b9372afe22fe71c851e90f54b63ad
kiokoeric <kiokoerick040@gmail.com>
2024-02-27 15:34:27 +0300

    Client Side Changes

--- Client/ebudget/src/Components/ChartImage/ChartImage.jsx
+++ Client/ebudget/src/Components/ChartImage/ChartImage.jsx
@@ -70,7 +70,7 @@ const ChartImage = () => {
             ],
             hoverOffset: 4,
             borderRadius: 5, 
-            spacing: 10,
+            spacing: 0,
         }]},
     
         options: {
